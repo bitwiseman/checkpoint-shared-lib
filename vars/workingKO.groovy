5d796955083f22e2cb977aa0509ce5ba737ca201
Alejandro Alvarez <jandroav@icloud.com>
2021-07-01 08:38:41 +0200

    Update workingKO.groovy

--- vars/workingKO.groovy
+++ vars/workingKO.groovy
@@ -1,41 +1,35 @@
 def call(parameterMap) {
-    node("pod") {
-      stage("Checkout code") {
-        Map tempParameterMap = [: ]
-        sh("echo 'testContent' >> file.txt")
-        stash name: "infra-files", includes: "*.txt", allowEmpty: true, useDefaultExcludes: false
-      }
-      function1()
-      function2()
+  node("pod") {
+    stage("Checkout code") {
+      Map tempParameterMap = [: ]
+      sh("echo 'testContent' >> file.txt")
+      stash name: "infra-files", includes: "*.txt", allowEmpty: true, useDefaultExcludes: false
     }
+    stage('Paralel inside node') {
+      parallel([
+        hello: {
+          echo "hello"
+        },
+        world: {
+          echo "world"
+        }
+      ])
+    }
+  }
+  function1()
 }
 
-    def function1() {
-      checkpoint("Start prod env deployment")
+def function1() {
+  checkpoint("Start prod env deployment")
 
-      stage("Milestone for prod deployment") {
-        milestone 1
-      }
+  stage("Milestone for prod deployment") {
+    milestone 1
+  }
 
-      node("pod") {
-        unstash "infra-files"
-        stage("Manual step for deploying changes to staging") {
-          sh("cat file.txt")
-        }
-      }
+  node("pod") {
+    unstash "infra-files"
+    stage("Manual step for deploying changes to staging") {
+      sh("cat file.txt")
     }
-
-    def function2() {
-      return node("pod") {
-        stage('Paralel inside node') {
-          parallel([
-            hello: {
-              echo "hello"
-            },
-            world: {
-              echo "world"
-            }
-          ])
-        }
-      }
-    }
+  }
+}
