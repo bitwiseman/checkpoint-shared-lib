820e6c62235b6fcbd2b5e450459e69402360907a
Alejandro Alvarez <jandroav@icloud.com>
2021-07-12 17:34:18 +0200

    Create GroovyClass.groovy

---
+++ vars/GroovyClass.groovy
@@ -0,0 +1,21 @@
+import package_java.*
+
+
+class executeJava implements Serializable{
+
+  JavaGroovy javaGroovy = new JavaGroovy();
+
+
+  /**
+   * no argument Constructor. This would invoke while an object of this class is created without passing any arguments.
+   */
+  executeJava(){
+  }
+
+  def setJava(){
+    javaGroovy.mul()
+  }
+
+
+
+ }
