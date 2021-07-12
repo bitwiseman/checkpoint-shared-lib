e30c4116c37398d813aa65c6bf7135b5eee145b0
Alejandro Alvarez <jandroav@icloud.com>
2021-07-12 18:09:54 +0200

    Create JavaGroovy.java

---
+++ src/io/stage/JavaGroovy.java
@@ -0,0 +1,11 @@
+package main.java.test;
+
+import java.io.Serializable;
+
+public class JavaGroovy implements Serializable {
+
+
+  public void mul (){
+    System.out.println("Java function is called");
+  }
+}
