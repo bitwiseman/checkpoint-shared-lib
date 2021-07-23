071f2356c1eaf6e92e690b9cc83b9dd3594011b9
Alejandro Alvarez <jandroav@icloud.com>
2021-07-23 16:05:16 +0200

    Update workingOK.groovy

--- vars/workingOK.groovy
+++ vars/workingOK.groovy
@@ -1,41 +1,60 @@
 def call(parameterMap) {
-  node("pod") {
-    stage("Checkout code") {
-      Map tempParameterMap = [: ]
-      sh("echo 'testContent' >> file.txt")
-      stash name: "infra-files", includes: "*.txt", allowEmpty: true, useDefaultExcludes: false
+    node('pod') {
+        container('pod') {
+            stage('Checkout code') {
+                sh('mkdir infra')
+                dir('infra') {
+                    sh("echo 'testContent' >> file.txt")
+                    echo('Validating infra.yml')
+                }
+                stash name: 'infra-files', includes: 'infra/**', allowEmpty: true, useDefaultExcludes: false
+            }
+            //Map<String, Closure> dryRunStages = addInfraDryRunStages()
+            //parallel dryRunStages
+        }
     }
-  }
-  function1()
-  function2()
+    Map<String, Closure> dryRunStages = addInfraDryRunStages()
+    parallel dryRunStages
+    function1()
 }
+
+def addInfraDryRunStages(){
+    Map<String, Closure> dryRunStages = [:]
+    Map tempParameterMap = prepareInfraDryRunStages()
+    dryRunStages.putAll(tempParameterMap)
+    return dryRunStages
+}
+
+
+def prepareInfraDryRunStages(){
+    parallelStage =  ["Dry run for": {
+        stage("Executing infra dry run for "){
+            dir("infra"){
+                sh(label: 'Creating aws directory', script: "mkdir -p test", returnStatus:false)
+                echo("Deleting aws directory")
+                echo("Preparing aws credentials")
+            }
+        }
+    }]
+    return parallelStage
+}
+
 
 def function1() {
-  checkpoint("Start prod env deployment")
+    checkpoint('Start prod env deployment')
 
-  stage("Milestone for prod deployment") {
-    milestone 1
-  }
-
-  node("pod") {
-    unstash "infra-files"
-    stage("Manual step for deploying changes to staging") {
-      sh("cat file.txt")
+    stage('Milestone for prod deployment') {
+        milestone 1
     }
-  }
-}
 
-def function2() {
-  return node("pod") {
-    stage('Paralel inside node') {
-      parallel([
-        hello: {
-          echo "hello"
-        },
-        world: {
-          echo "world"
+    node('pod') {
+        container('pod') {
+            unstash 'infra-files'
+            stage('Manual step for deploying changes to staging') {
+                dir('infra') {
+                    sh('cat file.txt')
+                }
+            }
         }
-      ])
     }
-  }
 }
