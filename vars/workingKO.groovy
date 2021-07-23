41e7a6e8dd5754ac70881848b57741fba20b044c
Alejandro Alvarez <jandroav@icloud.com>
2021-07-23 15:51:02 +0200

    Update workingKO.groovy

--- vars/workingKO.groovy
+++ vars/workingKO.groovy
@@ -1,42 +1,45 @@
 def call(parameterMap) {
-  node("pod") {
-    stage("Checkout code") {
-      Map tempParameterMap = [: ]
-      sh("mkdir infra")
-        dir("infra"){
-            sh("echo 'testContent' >> file.txt")
-            echo("Validating infra.yml")
-        }
-      stash name: "infra-files", includes: "infra/**", allowEmpty: true, useDefaultExcludes: false
-
-    }
-    stage('Paralel inside node') {
-      parallel([
+    node('pod') {
+        container('pod') {
+            stage('Checkout code') {
+                Map tempParameterMap = [: ]
+                sh('mkdir infra')
+                dir('infra') {
+                    sh("echo 'testContent' >> file.txt")
+                    echo('Validating infra.yml')
+                }
+                stash name: 'infra-files', includes: 'infra/**', allowEmpty: true, useDefaultExcludes: false
+            }
+            stage('Paralel inside node') {
+                parallel([
         hello: {
-          echo "hello"
+          echo 'hello'
         },
         world: {
-          echo "world"
+          echo 'world'
         }
       ])
+            }
+        }
     }
-  }
-  function1()
+    function1()
 }
 
 def function1() {
-  checkpoint("Start prod env deployment")
+    checkpoint('Start prod env deployment')
 
-  stage("Milestone for prod deployment") {
-    milestone 1
-  }
+    stage('Milestone for prod deployment') {
+        milestone 1
+    }
 
-  node("pod") {
-    unstash "infra-files"
-    stage("Manual step for deploying changes to staging") {
-        dir("infra"){
-            sh("cat file.txt")
+    node('pod') {
+        container('pod') {
+            unstash 'infra-files'
+            stage('Manual step for deploying changes to staging') {
+                dir('infra') {
+                    sh('cat file.txt')
+                }
+            }
         }
     }
-  }
 }
