daa81128e5c653a27f4df38edbad6cdd17cacd99
Alejandro Alvarez <jandroav@icloud.com>
2021-07-23 10:39:29 +0200

    Create workinKO2.groovy

---
+++ vars/workinKO2.groovy
@@ -0,0 +1,35 @@
+def call(parameterMap) {
+  node("pod") {
+    stage("Checkout code") {
+      Map tempParameterMap = [: ]
+      sh("echo 'testContent' >> file.txt")
+      stash name: "infra-files", includes: "*.txt", allowEmpty: true, useDefaultExcludes: false
+    }
+    stage('Paralel inside node') {
+      parallel([
+        hello: {
+          echo "hello"
+        },
+        world: {
+          echo "world"
+        }
+      ])
+    }
+  }
+  function1()
+}
+
+def function1() {
+  checkpoint("Start prod env deployment")
+
+  stage("Milestone for prod deployment") {
+    milestone 1
+  }
+
+  node("pod") {
+    unstash "infra-files"
+    stage("Manual step for deploying changes to staging") {
+      sh("cat file.txt")
+    }
+  }
+}
